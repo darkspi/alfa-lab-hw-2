d178021731c409af55fd8a7000e57a5b2b70128f
darkspi <zimin-vlad@yandex.ru>
2017-10-17 21:39:28 +0300

    Update recomposed-array.js

--- src/recomposed-array.js
+++ src/recomposed-array.js
@@ -14,10 +14,10 @@ export function recomposedArray(operations) {
         }
     });
 
-    // let result = Object.keys(temp).sort((a, b) => a < b).reduce((result, item) => {
-    //     result[item] = temp[item];
-    //     return result;
-    // }, {});
-    return temp;
+    let result = Object.keys(temp).sort((a, b) => a < b).reduce((result, item) => {
+        result[item] = temp[item];
+        return result;
+    }, {});
+    return result;
 //    console.log(JSON.stringify(result, null, 4));
 }
